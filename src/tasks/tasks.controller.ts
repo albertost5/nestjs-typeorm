e8641d46514a34136295de584bea49612ebafd77
albertost5 <sancheztorresalberto@gmail.com>
2022-11-03 14:08:55 +0100

    Add CRUD for tasks with no DB

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { query } from 'express';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
+
+    @Post()
+    create(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.tasksService.create(createTaskDto);
+    }
+
+    // @Post()
+    // create(@Body('title') title: string, @Body('description') description: string): Task {
+    //     return this.tasksService.create(title, description);
+    // }
+
+    // @Post('v2')
+    // create_v2(@Body() payload: any): Task {
+    //     return this.tasksService.create(payload.title, payload.description);
+    // }
+
+    @Get()
+    find(@Query() filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        if( !status && !search ) return this.tasksService.find();
+        
+        return this.tasksService.findWithFilters(filterDto);
+    }
     
+    @Get(':id')
+    findOne(@Param('id') id: string): Task {
+        return this.tasksService.findOne(id);
+    }
+
+    @Patch(':id/status')
+    updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): object {
+        return this.tasksService.delete(id)
+    }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    private tasks: Task[] = [];
+
+    create(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
+        const task: Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+
+        this.tasks.push(task)
+        return task;
+    }
+
+    // create(title: string, description: string): Task {
+    //     const task: Task = {
+    //         id: uuidv4(),
+    //         title,
+    //         description,
+    //         status: TaskStatus.OPEN
+    //     }
+
+    //     this.tasks.push(task)
+    //     return task;
+    // }
+
+    find(): Task[] {
+        return this.tasks;
+    }
+
+    findOne(id: string): Task {
+        return this.tasks.find(t => t.id === id);
+    }
+
+    findWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let allTasks = this.find();
+
+        if( status ) allTasks = allTasks.filter( t => t.status === status );
+        if( search ) allTasks = allTasks.filter( t => (t.description.includes(search) || t.title.includes(search)) );
+
+        return allTasks;
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.findOne(id);
+        task.status = status;
+        return task;
+    }
+
+    delete(id: string): object {
+        this.tasks = this.tasks.filter(t => t.id !== id);
+        return {
+            message: 'Task deleted successfully!'
+        };
+    }
+}
